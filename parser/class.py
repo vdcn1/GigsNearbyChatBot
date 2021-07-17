5e0c0ee267ae50d7fc021292692336e8e9435598
vdcn1 <veetsirrah@gmail.com>
2021-07-16 22:29:55 -0300

    parsing correctly

--- parser/class.py
+++ parser/class.py
@@ -1,8 +1,24 @@
 import json
-
+import sys
 # Loads the json file
-f_in = open('dialogues_001.json')
-multi_woz = json.loads(f_in)
+f_in = open('dialogues_001.json', 'r')
+multi_woz = json.loads(f_in.read())
 
-f_out = open('out')
-f.write(multi_woz)
+orig_stdout = sys.stdout
+
+with open('dataset1.csv', 'w') as f:
+    sys.stdout = f
+
+    print("ActiveIntent,Utterance")
+    for line in multi_woz:
+        if 'attraction' in line["services"]:
+            for turn in line["turns"]:
+                for frame in turn["frames"]:
+                    if 'attraction' in frame["service"]:
+                        if 'state' in frame:
+                            if 'active_intent' in frame['state']:
+                                if frame['state']['active_intent'] != 'NONE':
+                                    print(frame['state']['active_intent'], end=',')
+                                    if turn['speaker'] == 'USER':
+                                        print('"{}"'.format(turn['utterance']))
+    sys.stdout = orig_stdout
